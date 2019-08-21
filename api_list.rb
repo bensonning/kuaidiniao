ad1bc382b61d0d2ab0d70dc0e9bbc0126dddb3d9
bensonning <84756214@qq.com>
2019-08-21 16:43:00 +0800

    Update api_list.rb
    更新正式 测试地址

--- api_list.rb
+++ api_list.rb
@@ -1,26 +1,28 @@
-debug_api = [
+debug_api =  
     {
-        "id": 1,
-        "name": "即时查询API",
-        "code": "1002",
-        "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
-    },{
-        "id": 2,
-        "name": "电子面单API",
-        "code": "1007",
-        "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
+        "EbusinessOrderHandle": {
+            "name": "即时查询API",
+            "code": "1002",
+            "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
+         },
+        "EOrderService":{
+            "name": "电子面单API",
+            "code": "1007",
+            "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
+         }
+        
     }
-]
-api = [
-    {
-        "id": 1,
-        "name": "即时查询API",
-        "code": "1002",
-        "url": "http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx"
-    },{
-        "id": 2,
-        "name": "电子面单API",
-        "code": "1007",
-        "url": "http://api.kdniao.com/api/EOrderService"
+ 
+api =  {
+        "EbusinessOrderHandle": {
+            "name": "即时查询API",
+            "code": "1002",
+            "url": "http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx"
+         },
+        "EOrderService":{
+            "name": "电子面单API",
+            "code": "1007",
+            "url": "http://api.kdniao.com/api/EOrderService"
+         }
+        
     }
-]
