131d72e671eac975a0ee1cb5a71acf369a104fe9
bensonning <84756214@qq.com>
2019-08-21 16:21:49 +0800

    Create api_list.rb
    添加 api url

---
+++ api_list.rb
@@ -0,0 +1,26 @@
+debug_api = [
+    {
+        "id": 1,
+        "name": "即时查询API",
+        "code": "1002",
+        "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
+    },{
+        "id": 2,
+        "name": "电子面单API",
+        "code": "1007",
+        "url": "http://sandboxapi.kdniao.com:8080/kdniaosandbox/gateway/exterfaceInvoke.json"
+    }
+]
+api = [
+    {
+        "id": 1,
+        "name": "即时查询API",
+        "code": "1002",
+        "url": "http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx"
+    },{
+        "id": 2,
+        "name": "电子面单API",
+        "code": "1007",
+        "url": "http://api.kdniao.com/api/EOrderService"
+    }
+]
